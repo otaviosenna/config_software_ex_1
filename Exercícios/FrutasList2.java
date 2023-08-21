b2b5579e6f28a1ea64ff4df2000935250a0fdb7c
otaviosenna <otavio@sharehunter.com.br>
2023-08-20 22:17:26 -0300

    Subindo a segunda lista de frutas

---
+++ Exercícios/FrutasList2.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class FrutasList2 {
+    public static ArrayList<String> getFrutas() {
+        ArrayList<String> frutas = new ArrayList<>();
+        frutas.add("Mirtilo");
+        frutas.add("Morango");
+        frutas.add("Amora");
+        frutas.add("Maçã");
+        frutas.add("Laranja");
+        frutas.add("Banana");
+        frutas.add("Pera");
+        frutas.add("Jaca");
+        frutas.add("Melancia");
+        frutas.add("Maçã");
+        return frutas;
+    }
+}
