91b935d8375bd50743734ea6f0f212e7ac2d2124
LUCAS BIANCHESSI <l.bianchessi@facin.portoalegre.pucrsnet.br>
2023-08-22 21:49:10 -0300

    metodo retorna uniao adicionado, Exercicio E.

---
+++ Exercícios/RetornaUniao.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class RetornaUniao{
+    public static ArrayList<int> Uniao(ArrayList<int> l1, Arraylist<int> l2){
+        for (int i = 0; i < l2.size(); i++) {
+            l1.add(l2.get(i));
+        }
+        return l1;
+    }
+}
